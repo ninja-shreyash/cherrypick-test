e50597f82df7bc1fb830ff75bc41552452f7b544
GitHub Action <action@github.com>
2026-02-17 17:07:35 +0530

    Add environment support and email validation

--- config.go
+++ config.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 // Config holds application configuration.
 type Config struct {
@@ -8,24 +11,31 @@ type Config struct {
 	AppVersion string
 	DebugMode  bool
 	LogLevel   string
+	Env        string
 }
 
 // NewConfig creates a default configuration.
 func NewConfig() *Config {
+	env := os.Getenv("APP_ENV")
+	if env == "" {
+		env = "development"
+	}
 	return &Config{
 		AppName:    "myapp",
 		AppVersion: "1.0.1-release",
 		DebugMode:  false,
 		LogLevel:   "info",
+		Env:        env,
 	}
 }
 
 // PrintConfig outputs the configuration details.
 func PrintConfig(c *Config) {
-	fmt.Printf("App: %s v%s (debug=%v, log=%s)\n", c.AppName, c.AppVersion, c.DebugMode, c.LogLevel)
+	fmt.Printf("App: %s v%s (env=%s, debug=%v, log=%s)\n", c.AppName, c.AppVersion, c.Env, c.DebugMode, c.LogLevel)
 }
 
 func main() {
 	c := NewConfig()
 	PrintConfig(c)
+	fmt.Printf("Running in %s mode\n", c.Env)
 }
